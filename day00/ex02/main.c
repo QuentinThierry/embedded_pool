da6c1818f1badb87cdf9911e32003bc08c4de457
QuentinThierry <qthierry2@gmail.com>
2024-04-15 15:28:48 +0200

    [ADD] ex02

---
+++ day00/ex02/main.c
@@ -0,0 +1,20 @@
+#include <avr/io.h>
+#include <util/delay.h>
+
+int main()
+{
+	DDRB	=	(1 << PB0); // set pin PB0 of port B as output
+	DDRD	=	(0 << PD2); // set pin PD2 of port D as input
+	while (1)
+	{
+		if ((PIND & (1 << PD2)) == 0) // if the PD2 switch button is pressed
+		{
+			PORTB	=	(1 << PB0); // to write data on pin PB0 port B 
+		}
+		else if (PORTB & (1 << PB0)) // if the PD2 is not pressed and led D1 is on
+		{
+			PORTB	=	(0 << PB0); // to write data on port B pin 0
+		}
+		_delay_ms(10); // adding delay of 10 ms to prevent overheat
+	}
+}
