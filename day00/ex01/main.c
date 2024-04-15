29937b8e5967cbfcd94e4951780572fc91175a9e
QuentinThierry <qthierry2@gmail.com>
2024-04-15 20:02:21 +0200

    [FIX] use |= to keep previous data

--- day00/ex01/main.c
+++ day00/ex01/main.c
@@ -2,6 +2,6 @@
 
 int main()
 {
-	DDRB	=	(1 << PB0); // set pin 0 of port B as output
-	PORTB	=	(1 << PB0); // to write data on port B pin 0
+	DDRB	|=	(1 << PB0); // set pin 0 of port B as output
+	PORTB	|=	(1 << PB0); // to write data on port B pin 0
 }
