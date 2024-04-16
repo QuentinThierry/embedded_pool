0539cd6c50e56b9dbc4ff52a45b93dc0375a0f8d
QuentinThierry <qthierry2@gmail.com>
2024-04-16 17:46:13 +0200

    [ADD] ex01

---
+++ day01/ex01/main.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <util/delay.h>
+
+int main()
+{
+
+	SET(DDRB, PB1); // set PB1 to output
+
+	// prescaler -> doc : 16.11.2
+	SET(TCCR1B, CS12); // set prescaling to F_CPU/256 -> 62500 bit 1
+	RESET(TCCR1B, CS11); // set prescaling to F_CPU/256 -> 62500 bit 2
+	RESET(TCCR1B, CS10); // set prescaling to F_CPU/256 -> 62500 bit 3
+
+	// compare match -> doc : 16.11.1 - table 16-1
+	RESET(TCCR1A, COM1A1); // set toggle OC1A on compare match bit 1
+	SET(TCCR1A, COM1A0); // // set toggle OC1A on compare match bit 2
+
+	// set counter mode operation -> doc : 16.11.1 - table 16-4
+	RESET(TCCR1B, WGM13);
+	SET(TCCR1B, WGM12);
+	RESET(TCCR1A, WGM11);
+	RESET(TCCR1A, WGM10);
+
+	OCR1A = 31250;
+
+	while (1);
+}
