bad0c1c2ff0a71dcfe6b48f080c1ce925ad7b05a
QuentinThierry <qthierry2@gmail.com>
2024-04-24 17:43:15 +0200

    [ADD] ex02

---
+++ day07/ex02/main.c
@@ -0,0 +1,58 @@
+#include "main.h"
+
+void	init_ADC()
+{
+
+	//enable ADC
+	SET(ADCSRA, ADEN);
+
+	// mode 10 bit
+	RESET(ADMUX, ADLAR);
+
+	// set voltage reference to AVcc
+	RESET(ADMUX, REFS1);
+	SET(ADMUX, REFS0);
+
+	// prescaler
+	SET(ADCSRA, ADPS2);
+	SET(ADCSRA, ADPS1);
+	SET(ADCSRA, ADPS0);
+}
+
+void	select_channel(uint8_t val)
+{
+	// select channel
+	ADMUX &= 0xf0;
+	ADMUX |= val & 0x3;
+}
+
+void	print_val()
+{
+	SET(ADCSRA, ADSC);
+	while ((ADCSRA & (1 << ADSC)) != 0)
+		;
+	uart_int16(ADC);
+}
+
+
+int	main()
+{
+	uart_init();
+
+	init_ADC();
+
+	while (1)
+	{
+		select_channel(0);
+		print_val();
+		uart_printstr(" ");
+		select_channel(1);
+		print_val();
+		uart_printstr(" ");
+		select_channel(2);
+		print_val();
+		uart_printstr(" ");
+		_delay_ms(20);
+		uart_printstr("\r\n");
+	}
+}
---
+++ day07/ex02/uart.c
@@ -0,0 +1,81 @@
+#include "main.h"
+
+void	uart_init()
+{
+	const uint16_t	baud = F_CPU / (UART_BAUDRATE * 16);
+	UBRR0H = (baud & 0xff00) >> 8; // set baudrate
+	UBRR0L = baud & 0xff;
+	SET(UCSR0B, TXEN0); // set USART mode to transmit
+	SET(UCSR0B, RXEN0); // set USART mode to recieve
+
+	RESET(UCSR0C, UMSEL01); // set asynchronous USART mode
+	RESET(UCSR0C, UMSEL00);
+
+	RESET(UCSR0C, UPM00); // disable parity mode for 8N1
+	RESET(UCSR0C, UPM01);
+
+	RESET(UCSR0C, USBS0); // set 1 stop bit
+
+	SET(UCSR0C, UCSZ00); // set 8 bit size
+	SET(UCSR0C, UCSZ01);
+	RESET(UCSR0B, UCSZ02);
+}
+
+void	uart_tx(char c)
+{
+	while (!(UCSR0A & (1 << UDRE0))); // wait data to be transmit
+	UDR0 = c;
+}
+
+void	uart_printstr(const char *str)
+{
+	uint16_t	i = 0;
+	while (str[i])
+	{
+		uart_tx(str[i]);
+		i++;
+	}
+}
+
+void	uart_printhex(uint8_t data)
+{
+	const char base[] = "0123456789ABCDEF";
+	// uart_printstr("0x");
+	uart_tx(base[(data & 0xf0) >> 4]);
+	uart_tx(base[(data & 0xf)]);
+}
+
+void	uart_printhex_word(uint16_t data)
+{
+	const char base[] = "0123456789ABCDEF";
+	// uart_printstr("0x");
+	uart_tx(base[(data & 0xf000) >> 12]);
+	uart_tx(base[(data & 0xf00) >> 8]);
+	uart_tx(base[(data & 0xf0) >> 4]);
+	uart_tx(base[(data & 0xf)]);
+}
+
+char	uart_rx(void)
+{
+	while (!(UCSR0A & (1 << RXC0)));
+	return UDR0;
+}
+
+char	uart_rtx()
+{
+	char c = uart_rx();
+	uart_tx(c);
+	return c;
+}
+
+
+void	uart_int16(uint16_t n)
+{
+	if (n >= 10)
+	{
+		uart_int16(n / 10);
+		uart_int16(n % 10);
+	}
+	else
+		uart_tx(n + '0');
+}
